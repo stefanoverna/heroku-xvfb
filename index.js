fb3f87b89b7f3a7f6cd0b7c74c9d25e0e105313a
Stefano Verna <s.verna@datocms.com>
2021-12-04 15:17:54 +0100

    Fix

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use('/static', express.static('/tmp'));
 app.get("/", (req, res) => res.send("Hello World!"));
 app.post("/scrape", async (req, res) => {
   const data = await runner('datocms');
